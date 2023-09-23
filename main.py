4d44eafcb1e9ceacd654b3746729f1b6a6eb17ba
sofiagakalo1 <sofiagakalo1@gmail.com>
2023-09-23 14:52:32 +0200

    added enemies and bonuses, opportunity to move player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
 import pygame
-from pygame.constants import QUIT
+from pygame.constants import QUIT, K_DOWN, K_UP, K_LEFT, K_RIGHT
 
 pygame.init()
 
@@ -10,6 +11,8 @@ WIDTH = 1200
 
 COLOR_WHITE = (255, 255, 255)
 COLOR_BLACK = (0, 0, 0)
+COLOR_BLUE = (0, 0, 255)
+COLOR_RED = (255, 0, 0)
 
 main_display = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -17,51 +20,97 @@ player_size = (20, 20)
 player = pygame.Surface(player_size)
 player.fill(COLOR_WHITE)
 player_rect = player.get_rect()
-player_speed = [1, 1]
+
+player_move_down = [0, 1]
+player_move_right = [1, 0]
+player_move_up = [0, -1]
+player_move_left = [-1, 0]
+
+# creating ENEMY
+def create_enemy():
+    enemy_size = (30, 30)
+    enemy = pygame.Surface(enemy_size)
+    enemy.fill(COLOR_BLUE)
+    enemy_rect = pygame.Rect(WIDTH, random.randint(0, HEIGHT), *enemy_size)
+    enemy_move = [random.randint(-6, -1), 0]
+    return [enemy, enemy_rect, enemy_move]
+
+
+CREATE_ENEMY = pygame.USEREVENT+1
+pygame.time.set_timer(CREATE_ENEMY, 1500)
+
+enemies = []
+
+# creating BONUS
+
+def create_bonus():
+    bonus_size = (50, 50)
+    bonus = pygame.Surface(bonus_size)
+    bonus.fill(COLOR_RED)
+    bonus_rect = pygame.Rect(random.randint(0, WIDTH), 0, *bonus_size)
+    bonus_move = [0, random.randint(1, 6)]
+    return [bonus, bonus_rect, bonus_move]
+
+CREATE_BONUS = pygame.USEREVENT+2
+pygame.time.set_timer(CREATE_BONUS, 2500)
+
+bonuses = []
+#
 
 playing = True
 
 while playing:
 
-    FPS.tick(1200)
+    FPS.tick(250)
 
     for event in pygame.event.get():
         if event.type == QUIT:
             playing = False
+        if event.type == CREATE_ENEMY:
+            enemies.append(create_enemy())
+        if event.type == CREATE_BONUS:
+            bonuses.append(create_bonus())
 
     main_display.fill(COLOR_BLACK)
 
-    if player_rect.bottom >= HEIGHT:
-        player_speed[1] = -player_speed[1]
+    keys = pygame.key.get_pressed()
 
-    if player_rect.right >= WIDTH:
-        player_speed[0] = -player_speed[0]
+# обробка клавіш
+    if keys[K_DOWN] and player_rect.bottom < HEIGHT:
+        player_rect = player_rect.move(player_move_down)
 
-    if player_rect.top <0:
-        player_speed[1] = -player_speed[1]
+    if keys[K_RIGHT] and player_rect.right < WIDTH:
+        player_rect = player_rect.move(player_move_right)
 
-    if player_rect.left <0:
-      player_speed[0] = -player_speed[0]
+    if keys[K_UP] and player_rect.top < HEIGHT:
+        player_rect = player_rect.move(player_move_up)
 
-    # if player_rect.bottom >= HEIGHT:
-    #     player_speed = [1, -1]
+    if keys[K_LEFT] and player_rect.left < WIDTH:
+        player_rect = player_rect.move(player_move_left)
 
-    # if player_rect.right >= WIDTH:
-    #     player_speed = [-1, -1]
+# showing enemies on screen
+    for enemy in enemies:
+        enemy[1] = enemy[1].move(enemy[2])
+        main_display.blit(enemy[0], enemy[1])
 
-    # if player_rect.top <0:
-    #     player_speed = [-1, 1]
-
-    # if player_rect.left <0:
-    #     player_speed = [1, 1]
-
-    # print(player_rect.bottom) moving to the bottom till 1300
+# showing bonuses on screen
+    for bonus in bonuses:
+        bonus[1] = bonus[1].move(bonus[2])
+        main_display.blit(bonus[0], bonus[1])
+#
 
     main_display.blit(player, player_rect)
 
-    player_rect = player_rect.move(player_speed)
-
     pygame.display.flip()
 
+# clearing massive depends on actual ENEMIES on screen
+    for enemy in enemies:
+        if enemy[1].left < 0:
+            enemies.pop(enemies.index(enemy))
+
+# clearing massive depends on actual BONUSES on screen
+    for bonus in bonuses:
+        if bonus[1].left < 0:
+            bonuses.pop(bonuses.index(bonus))
 
 # python3 main.py
